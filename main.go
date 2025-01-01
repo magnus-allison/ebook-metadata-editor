20ced0592d7c6c00446a9140d60a86ddccb7b191
Magnus <magnus_alli@hotmail.co.uk>
2025-01-01 12:45:44 +0000

    initial

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"ebook-meta-editor/dialogs"
+	"fmt"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
 )
 
 func main() {
 
 	app := app.New()
 	app.Settings().SetTheme(NewTheme())
-	window := app.NewWindow("Epub Editor")
+	window := app.NewWindow(" ")
+	drv := fyne.CurrentApp().Driver()
+	if drv, ok := drv.(desktop.Driver); ok {
+		fmt.Println("Driver is desktop", drv)
+		// window = drv.CreateSplashWindow()
+	}
 	window.Resize(fyne.NewSize(800, 600))
+	// borderless
+	window.CenterOnScreen()
 
 	gui := &GUI{
 		window: window,
